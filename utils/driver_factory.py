2e5aef6436fcf84d1d86b42aafe61c3aed424450
Artur Strug <artstrug@gmail.com>
2021-04-21 21:02:51 +0200

    Add config.json, extend driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -2,15 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from helpers.web_driver_listener import WebDriverListener
+from msedge.selenium_tools import EdgeOptions, Edge
 
 
 class DriverFactory:
     @staticmethod
-    def get_driver(browser) -> EventFiringWebDriver:
+    def get_driver(browser, headless_mode=False) -> EventFiringWebDriver:
         if browser == "chrome":
             options = webdriver.ChromeOptions()
             options.add_argument("start-maximized")
+            if headless_mode is True:
+                options.add_argument("--headless")
             driver = EventFiringWebDriver(
                 webdriver.Chrome(ChromeDriverManager().install(), options=options),
                 WebDriverListener()
@@ -18,9 +22,21 @@ class DriverFactory:
             return driver
         elif browser == "firefox":
             options = webdriver.FirefoxOptions()
-            options.add_argument("start-maximized")
+            if headless_mode is True:
+                options.headless = True
             driver = EventFiringWebDriver(
                 webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options),
+                WebDriverListener()
+            )
+            return driver
+        elif browser == "edge":
+            options = EdgeOptions()
+            options.use_chromium = True
+            if headless_mode is True:
+                options.headless = True
+            driver_path = EdgeChromiumDriverManager().install()
+            driver = EventFiringWebDriver(
+                Edge(executable_path=driver_path, options=options),
                 WebDriverListener()
             )
             return driver
